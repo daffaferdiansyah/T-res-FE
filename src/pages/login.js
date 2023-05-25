7d9e5987bfc715ee9720391b4ebee281f7dbd0c4
Shidqi Yusri <shidqifadhlu@gmail.com>
2023-05-25 21:56:10 +0700

    tambah call api buat login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logoputih from '../assets/logo-white-removebg-preview 2.png'
 import logotelkom from '../assets/telkom-university-logo.png'
+import axios from 'axios';
+import Swal from "sweetalert2";
+
+
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [errorMessages, setErrorMessages] = useState([]);
+
+    useEffect(() => {
+        let data = localStorage.getItem('data');
+        if(data !== null){
+            window.location.href = "home"
+        }
+    }, []);
+
+    const formHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8080/user/login', {
+            username: username,
+            password: password,
+        }).then((res) => {  
+            localStorage.setItem('data', JSON.stringify(res.data));
+            setUsername('');
+            setPassword('');
+            return Swal.fire({
+                heightAuto: false,
+                icon: "success",
+                title: "Berhasil",
+                text: "Akun berhasil login",
+                confirmButtonColor: "#8B5CF6",
+                confirmButtonText: "Ok",
+            }).then((res) => {
+                if (res.isConfirmed) window.location.href = "/homepage";
+            });
+            
+        }).catch((error) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Username or password invalid',
+              })
+            setPassword('');
+
+            
+        });
+    }
   return (
     <div>
         <div className='flex h-screen'>
@@ -14,14 +61,14 @@ function Login() {
             </div>
             <div className='w-1/2 bg-primary bg-no-repeat bg-center' style={{backgroundImage: `url(${logotelkom})`}}>
                 <div className='flex justify-center items-center h-screen'  >
-                    <form>
+                    <form onSubmit={formHandler}>
                         <div class="mb-4">
                             <label for="username" class="block text-gray-700 font-semibold mb-2">Username</label>
-                            <input type="text" id="username" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter your username"/>
+                            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} id="username" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter your username"/>
                         </div>
                         <div class="mb-4">
                             <label for="password" class="block text-gray-700 font-semibold mb-2">Password</label>
-                            <input type="password" id="password" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter your password"/>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" class="w-full px-4 py-2 border border-gray-300 rounded" placeholder="Enter your password"/>
                         </div>
                         <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">Log In</button>
                     </form>
