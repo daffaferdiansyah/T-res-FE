3cc9dd8d38cfbcca368a1837dc2c41576cd61602
Shidqi Yusri <shidqifadhlu@gmail.com>
2023-05-30 19:58:49 +0700

    tambah datepicker

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import back from '../assets/left-arrow (2).png'
 import gsg from '../assets/gsg.jpeg'
+// import Datepicker from 'flowbite-datepicker/Datepicker';
+import flowbit from '../../node_modules/flowbite/dist/flowbite.min.js'
 // import duit from '../aset/saldo.png'
+// import {
+//   Datepicker,
+//   Input,
+//   initTE,
+// } from "tw-elements";
+
+// initTE({ Datepicker, Input });
+import Datepicker from "react-tailwindcss-datepicker";
+
 
 
 export default function Details() {
+  const [value, setValue] = useState({
+    startDate: new Date(),
+    endDate: new Date().setMonth(11)
+  });
+
+  const handleValueChange = (newValue) => {
+      console.log("newValue:", newValue);
+      setValue(newValue);
+  }
   return (
     <div>
+      <script src={flowbit}></script>
       <div className='flex bg-white py-2 w-full'>
         <div>
           <a href="/homepage" className='grid grid-cols-2 ml-2 mt-2'>
@@ -28,12 +49,10 @@ export default function Details() {
           <div className='grid grid-cols-3'>
             <div>
               
-<div class="relative max-w-sm">
-  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-  </div>
-  <input datepicker type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
-</div>
+            <Datepicker
+                value={value}
+                onChange={handleValueChange}
+            />
 
             </div>
             <div>b</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,6 +12,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
